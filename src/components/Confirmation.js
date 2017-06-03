09d518636084d3b3202331ec38b99a90d32b95e1
Uyiosa Enabulele <hiskonxeptz@gmail.com>
2017-06-03 11:48:16 +0100

    style confirmation button

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Confrimation = ({ addedProducts, onChangeRoute }) => (
-  <div>
-    Confirmation Page
-    <ul>
+  <div className="product-group">
+    <h3 className="category-header">Confirmation Page</h3>
+    <ul className="cart-items">
       {addedProducts.map(addedProduct => (
         <li> {addedProduct} </li>
     ))}
     </ul>
-    <button onClick={() => { onChangeRoute(false); }}>Goto Products </button>
+    <button className="confirm-btn btn" onClick={() => { onChangeRoute(false); }}>Goto Products </button>
   </div>
         );
 
